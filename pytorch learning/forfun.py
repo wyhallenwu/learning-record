3afd2383be79d66de3ad4d7151ee33b8a988181d
wyhallenwu <wyhforwork66@gmail.com>
2021-12-13 20:10:38 +0800

    fun coding

--- pytorch learning/forfun.py
+++ pytorch learning/forfun.py
@@ -2,11 +2,39 @@ import numpy as np
 import torch
 import torch.nn as nn
 x = torch.randn(10, 3, requires_grad = True)
-w = torch.randn(12, 10, requires_grad = True)
-b = torch.randn(12, 3,requires_grad = True)
+# w = torch.randn(12, 10, requires_grad = True)
+b = torch.randn(10, 10)
+
+y_ = np.random.uniform(0, 3, (100, 10, 10))
+print(y_)
+y = torch.from_numpy(y_).float()
 print(x)
-print(x.size(), w.size(), b.size())
-y = torch.mm(w, x) + b
-print(y)
+print(y.type(), b.size())
+# y = torch.mm(w, x) + b
+# prsint(y)
 
+# model 
+model = nn.Linear(3, 10)
 
+# optimizer
+optimizer = torch.optim.SGD(model.parameters(), lr = 1e-3)
+
+# loss
+loss = nn.MSELoss()
+
+def forward(x):
+    return model(x) + b
+
+pred_y = forward(x)
+
+pred_loss = loss(pred_y, y)
+
+epoch = 10
+batch_size = 10
+
+for i in range(10):
+    optimizer.step()
+    pred_y = forward(x)
+    pred_loss = loss(pred_y, y)
+    pred_loss.backward()
+    print(f'loss after {i} optim is: {pred_loss}')
